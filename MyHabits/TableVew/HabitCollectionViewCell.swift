e5d33011f4573a48cd0c66a5dd3207ead8af0905
Tatiana Zhukova <tatiana.s.zhukova@yandex.ru>
2023-04-25 19:15:54 +0500

    add HabitCollectionViewCell

---
+++ MyHabits/TableVew/HabitCollectionViewCell.swift
@@ -0,0 +1,121 @@
+//
+//  HabitCollectionViewCell.swift
+//  MyHabits
+//
+//  Created by Татьяна Жукова on 25.04.2023.
+//
+
+import UIKit
+
+class HabitCollectionViewCell: UICollectionViewCell {
+
+    private lazy var nameHabit: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        $0.numberOfLines = 2
+
+        return $0
+    }(UILabel())
+
+    private lazy var timeHabit: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textColor = UIColor(named: "Gray")
+        $0.font = .systemFont(ofSize: 12)
+
+        return $0
+    }(UILabel())
+
+    private lazy var counter: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textColor = .systemGray2
+        $0.font = .systemFont(ofSize: 13)
+
+        return $0
+    }(UILabel())
+
+    lazy var checkMarkButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.cornerRadius = 20
+        $0.layer.borderWidth = 2.5
+        $0.addTarget(self, action: #selector(executionHabit), for: .touchUpInside)
+        $0.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        $0.tintColor = .white
+
+        return $0
+    }(UIButton())
+
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+
+        setupUI()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        nameHabit.text = nil
+        nameHabit.textColor = nil
+        timeHabit.textColor = nil
+        checkMarkButton.layer.borderColor = nil
+        checkMarkButton.layer.backgroundColor = nil
+        counter.text = nil
+    }
+
+    private func setupUI() {
+        contentView.addSubview(nameHabit)
+        contentView.addSubview(timeHabit)
+        contentView.addSubview(counter)
+        contentView.addSubview(checkMarkButton)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            checkMarkButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            checkMarkButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            checkMarkButton.heightAnchor.constraint(equalToConstant: 40),
+            checkMarkButton.widthAnchor.constraint(equalToConstant: 40),
+
+            nameHabit.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            nameHabit.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            nameHabit.trailingAnchor.constraint(equalTo: checkMarkButton.leadingAnchor, constant: -40),
+
+            timeHabit.topAnchor.constraint(equalTo: nameHabit.bottomAnchor, constant: 5),
+            timeHabit.leadingAnchor.constraint(equalTo: nameHabit.leadingAnchor),
+
+            counter.leadingAnchor.constraint(equalTo: nameHabit.leadingAnchor),
+            counter.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
+        ])
+    }
+
+    func setup(index: Int) {
+        nameHabit.tag = index
+        nameHabit.text = HabitsStore.shared.habits[index].name
+        nameHabit.textColor = HabitsStore.shared.habits[index].color
+        timeHabit.text = "Каждый день в \(HabitsStore.shared.habits[index].dateString)"
+        counter.text = "Счетчик: \(HabitsStore.shared.habits[index].trackDates.count)"
+        checkMarkButton.layer.borderColor = HabitsStore.shared.habits[index].color.cgColor
+
+        if HabitsStore.shared.habits[index].isAlreadyTakenToday {
+            checkMarkButton.backgroundColor = UIColor(cgColor: checkMarkButton.layer.borderColor ?? UIColor.white.cgColor)
+        } else {
+            checkMarkButton.backgroundColor = .white
+        }
+    }
+
+    @objc func executionHabit() {
+        let index = nameHabit.tag
+
+        if HabitsStore.shared.habits[index].isAlreadyTakenToday {
+            print("executionHabit")
+        } else {
+            checkMarkButton.backgroundColor = UIColor(cgColor: checkMarkButton.layer.borderColor ?? UIColor.white.cgColor)
+            HabitsStore.shared.track(HabitsStore.shared.habits[index])
+            counter.text = "Счетчик: \(HabitsStore.shared.habits[index].trackDates.count)"
+        }
+    }
+}
